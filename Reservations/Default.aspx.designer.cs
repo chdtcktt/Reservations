71081e437149a55b1e02668257951ee2db7514d7
chdtcktt <chdtcktt@gmail.com>
2014-01-08 20:50:30 -0800

    EOC Commit

--- Reservations/Default.aspx.designer.cs
+++ Reservations/Default.aspx.designer.cs
@@ -7,11 +7,27 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Reservations
-{
-
-
-    public partial class _Default
-    {
+namespace Reservations {
+    
+    
+    public partial class _Default {
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Reservations control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource Reservations;
     }
 }
